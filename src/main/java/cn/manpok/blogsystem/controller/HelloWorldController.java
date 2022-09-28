2e85f3f8b367e065b14fa47868a8a082a8e7106e
zhengwenbo <19802021891@139.com>
2022-09-28 15:17:04 +0800

    集成图形验证码库

--- src/main/java/cn/manpok/blogsystem/controller/HelloWorldController.java
+++ src/main/java/cn/manpok/blogsystem/controller/HelloWorldController.java
@@ -2,8 +2,14 @@ package cn.manpok.blogsystem.controller;
 
 import cn.manpok.blogsystem.pojo.User;
 import cn.manpok.blogsystem.response.ResponseResult;
+import com.pig4cloud.captcha.SpecCaptcha;
+import com.pig4cloud.captcha.base.Captcha;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 
 @Slf4j
 @RestController
@@ -20,5 +26,27 @@ public class HelloWorldController {
     public ResponseResult login(@RequestBody User user) {
         log.info("login success! ");
         return ResponseResult.SUCCESS().setData(user);
+    }
+
+    @GetMapping("/captcha")
+    public void getCaptcha(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // 设置请求头为输出图片类型
+        response.setContentType("image/gif");
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+
+        // 三个参数分别为宽、高、位数
+        SpecCaptcha specCaptcha = new SpecCaptcha(130, 48, 5);
+        // 设置字体
+        specCaptcha.setFont(new Font("Verdana", Font.PLAIN, 32));  // 有默认字体，可以不用设置
+        // 设置类型，纯数字、纯字母、字母数字混合
+        specCaptcha.setCharType(Captcha.TYPE_ONLY_NUMBER);
+
+        // 验证码存入session
+        request.getSession().setAttribute("captcha", specCaptcha.text().toLowerCase());
+
+        // 输出图片流
+        specCaptcha.out(response.getOutputStream());
     }
 }
