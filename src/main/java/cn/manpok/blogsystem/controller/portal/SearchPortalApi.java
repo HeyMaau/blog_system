86b342d3391a3bdbf3e455896c18c215bee68f69
zhengwenbo <19802021891@139.com>
2023-02-20 16:28:35 +0800

    修改搜索接口url参数

--- src/main/java/cn/manpok/blogsystem/controller/portal/SearchPortalApi.java
+++ src/main/java/cn/manpok/blogsystem/controller/portal/SearchPortalApi.java
@@ -1,11 +1,13 @@
 package cn.manpok.blogsystem.controller.portal;
 
-import cn.manpok.blogsystem.interceptors.CheckRepeatedCommit;
 import cn.manpok.blogsystem.response.ResponseResult;
 import cn.manpok.blogsystem.service.ISolrSearchService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 门户搜索API
@@ -25,9 +27,8 @@ public class SearchPortalApi {
      * @param page
      * @return
      */
-    @CheckRepeatedCommit
-    @GetMapping("/{keyword}")
-    public ResponseResult doSearch(@PathVariable("keyword") String keyword,
+    @GetMapping()
+    public ResponseResult doSearch(@RequestParam("keyword") String keyword,
                                    @RequestParam(value = "category_id", required = false) String categoryID,
                                    @RequestParam(value = "sort", required = false) Integer sort,
                                    @RequestParam("page") int page,
