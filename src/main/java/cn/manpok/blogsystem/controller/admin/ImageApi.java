3c8d497413895ac5b35c7a61319acf5680ceda2b
zhengwenbo <19802021891@139.com>
2023-02-23 14:50:16 +0800

    更新图片api

--- src/main/java/cn/manpok/blogsystem/controller/admin/ImageApi.java
+++ src/main/java/cn/manpok/blogsystem/controller/admin/ImageApi.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Slf4j
 @RestController
-@RequestMapping("/admin/image")
+@RequestMapping("/image")
 public class ImageApi {
 
     @Autowired
