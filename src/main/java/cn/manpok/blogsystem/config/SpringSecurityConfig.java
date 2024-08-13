e86202eb771701f1d2a0f67d46765d8f26db0620
zhengwenbo <19802021891@139.com>
2024-08-13 20:16:58 +0800

    feat: spring security配置更新

--- src/main/java/cn/manpok/blogsystem/config/SpringSecurityConfig.java
+++ src/main/java/cn/manpok/blogsystem/config/SpringSecurityConfig.java
@@ -1,8 +1,13 @@
 package cn.manpok.blogsystem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -12,10 +17,12 @@ public class SpringSecurityConfig {
     /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll().and().csrf().disable();
-    }
+    }*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.
-    }*/
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
 }
