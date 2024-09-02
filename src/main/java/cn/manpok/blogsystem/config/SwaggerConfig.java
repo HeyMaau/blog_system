f6c95656fdd6b4df81158b565e917b21dd8fe87d
manpok <841294180@163.com>
2024-09-02 15:09:07 +0800

    feat: 集成Springdoc

--- src/main/java/cn/manpok/blogsystem/config/SwaggerConfig.java
+++ src/main/java/cn/manpok/blogsystem/config/SwaggerConfig.java
@@ -1,99 +1,44 @@
 package cn.manpok.blogsystem.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(name = "blog.system.swagger.enable", havingValue = "true")
 public class SwaggerConfig {
-/*
-    *//**
-     * 创建用户API信息
-     *
-     * @return
-     *//*
+
     @Bean
-    public Docket createUserApi() {
-        return new Docket(DocumentationType.SWAGGER_2)  // DocumentationType.SWAGGER_2 固定的，代表swagger2
-                .groupName("用户API") // 如果配置多个文档的时候，那么需要配置groupName来分组标识
-                .apiInfo(userApiInfo()) // 用于生成API信息
-                .select() // select()函数返回一个ApiSelectorBuilder实例,用来控制接口被swagger做成文档
-                .apis(RequestHandlerSelectors.basePackage("cn.manpok.blogsystem.controller.user")) // 用于指定扫描哪个包下的接口
-                .paths(PathSelectors.any())// 选择所有的API,如果你想只为部分API生成文档，可以配置这里
+    public OpenAPI myOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("卧卷")
+                        .description("后台API文档")
+                        .version("v1.3"));
+    }
+
+    @Bean
+    public GroupedOpenApi portalApiInfo() {
+        return GroupedOpenApi.builder()
+                .group("门户API")
+                .pathsToMatch("/portal/**")
                 .build();
     }
 
-    *//**
-     * 用户API信息
-     *
-     * @return
-     *//*
-    private ApiInfo userApiInfo() {
-        return new ApiInfoBuilder()
-                .title("博客系统用户API") //  可以用来自定义API的主标题
-                .description("博客系统项目用户API管理") // 可以用来描述整体的API
-                .termsOfServiceUrl("") // 用于定义服务的域名
-                .version("1.0") // 可以用来定义版本。
-                .build(); //
-    }
-
-    *//**
-     * 创建管理API信息
-     *
-     * @return
-     *//*
     @Bean
-    public Docket createAdminApi() {
-        return new Docket(DocumentationType.SWAGGER_2)  // DocumentationType.SWAGGER_2 固定的，代表swagger2
-                .groupName("管理API") // 如果配置多个文档的时候，那么需要配置groupName来分组标识
-                .apiInfo(adminApiInfo()) // 用于生成API信息
-                .select() // select()函数返回一个ApiSelectorBuilder实例,用来控制接口被swagger做成文档
-                .apis(RequestHandlerSelectors.basePackage("cn.manpok.blogsystem.controller.admin")) // 用于指定扫描哪个包下的接口
-                .paths(PathSelectors.any())// 选择所有的API,如果你想只为部分API生成文档，可以配置这里
+    public GroupedOpenApi adminApiInfo() {
+        return GroupedOpenApi.builder()
+                .group("管理API")
+                .pathsToMatch("/admin/**")
                 .build();
     }
 
-    *//**
-     * 管理API信息
-     *
-     * @return
-     *//*
-    private ApiInfo adminApiInfo() {
-        return new ApiInfoBuilder()
-                .title("博客系统管理API") //  可以用来自定义API的主标题
-                .description("博客系统项目管理API管理") // 可以用来描述整体的API
-                .termsOfServiceUrl("") // 用于定义服务的域名
-                .version("1.0") // 可以用来定义版本。
-                .build(); //
-    }
-
-    *//**
-     * 创建门户API信息
-     *
-     * @return
-     *//*
     @Bean
-    public Docket createPortalApi() {
-        return new Docket(DocumentationType.SWAGGER_2)  // DocumentationType.SWAGGER_2 固定的，代表swagger2
-                .groupName("门户API") // 如果配置多个文档的时候，那么需要配置groupName来分组标识
-                .apiInfo(portalApiInfo()) // 用于生成API信息
-                .select() // select()函数返回一个ApiSelectorBuilder实例,用来控制接口被swagger做成文档
-                .apis(RequestHandlerSelectors.basePackage("cn.manpok.blogsystem.controller.portal")) // 用于指定扫描哪个包下的接口
-                .paths(PathSelectors.any())// 选择所有的API,如果你想只为部分API生成文档，可以配置这里
+    public GroupedOpenApi userApiInfo() {
+        return GroupedOpenApi.builder()
+                .group("用户API")
+                .pathsToMatch("/user/**")
                 .build();
     }
-
-    *//**
-     * 门户API信息
-     *
-     * @return
-     *//*
-    private ApiInfo portalApiInfo() {
-        return new ApiInfoBuilder()
-                .title("博客系统门户API") //  可以用来自定义API的主标题
-                .description("博客系统项目门户API管理") // 可以用来描述整体的API
-                .termsOfServiceUrl("") // 用于定义服务的域名
-                .version("1.0") // 可以用来定义版本。
-                .build(); //
-    }*/
 }
